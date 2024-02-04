7569330a2f359d3bbfc6ff38b64122dd808ac52c
Preet Singh <150048406+Preet2099x@users.noreply.github.com>
2024-02-04 12:57:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function getTime()
+{ 
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("hrs").innerHTML = hours;
+    document.getElementById("mins").innerHTML = minutes;
+    document.getElementById("secs").innerHTML = seconds;
+}
+
+getTime();
+setInterval(getTime, 1000);
+
